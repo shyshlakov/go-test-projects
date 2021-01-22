299783cae5086badab46954cfee8157e01d1f523
shyshlakov <shyshlakov@icloud.com>
2021-01-23 01:18:38 +0200

    example with arrays

--- array/sum.go
+++ array/sum.go
@@ -9,12 +9,26 @@ func Sum(numbers []int) int {
 	return sum
 }
 
-func SumAll(numbersToSum ...[]int) (sums []int) {
-	lengthOfNumbers := len(numbersToSum)
-	sums = make([]int, lengthOfNumbers)
+// SumAll takes arrays and returns new array
+func SumAll(numbersToSum ...[]int) []int {
+	var sums []int
+	for _, numbers := range numbersToSum {
+		sums = append(sums, Sum(numbers))
+	}
 
-	for i, numbers := range numbersToSum {
-		sums[i] = Sum(numbers)
+	return sums
+}
+
+// SumAllTails takes array...
+func SumAllTails(numbersToSum ...[]int) []int {
+	var sums []int
+	for _, numbers := range numbersToSum {
+		if len(numbers) == 0 {
+			sums = append(sums, 0)
+		} else {
+			tail := numbers[1:]
+			sums = append(sums, Sum(tail))
+		}
 	}
 
 	return sums
